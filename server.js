638298ef5ad84180005ce49ae3f95a8a76e92820
Alex Cocan <al_razvan@yahoo.com>
2020-11-18 17:09:43 +0100

    deploy to heroku

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ mongoose.connect(uri, {
     useUnifiedTopology: true
 });
 
-// if(process.env.NODE_ENV === "production"){
-//     app.use(express.static("client/build"))
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"))
 
-//     // app.get("*", (req,res) => {
-//     //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//     // })
-// }
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const connection = mongoose.connection;
 connection.once('open', ()=>
